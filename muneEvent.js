75b323874a3522456b8035ebe3d13b96eeb0b6a9
linzino <linzino7@gmail.com>
2019-10-18 16:47:32 +0800

    Add files via upload

---
+++ muneEvent.js
@@ -0,0 +1,43 @@
+var menuItem = { 
+    "id": "cambridge_search_plugin",
+    "title": "cambridge_search",
+    "contexts": ["selection"]
+};
+
+function openlink(keyword,url) {
+    // 取代
+    var word = keyword;
+    var re_word = word.toLowerCase().replace(/ /g, '-');
+    //合併成搜尋連結
+    var url =  url + re_word;
+    // 開啟新視窗
+    window.open(url, "dictWindow");
+}
+
+
+chrome.contextMenus.create(menuItem);
+
+chrome.contextMenus.onClicked.addListener(function (clickData) {
+    if(clickData.menuItemId == "cambridge_search_plugin" && clickData.selectionText) {
+
+        console.log('click');
+
+        // 進storage非同步抓取資料
+        chrome.storage.sync.get(['cs_language'], function(result) {
+
+            tmp = result.cs_language
+            //alert(tmp);
+
+            if(tmp == "english"){
+                var url_head = "https://dictionary.cambridge.org/dictionary/english/"
+                // alert("english");
+                openlink(clickData.selectionText, url_head);
+            }else{
+                var url_head = "https://dictionary.cambridge.org/zht/%E8%A9%9E%E5%85%B8/%E8%8B%B1%E8%AA%9E-%E6%BC%A2%E8%AA%9E-%E7%B9%81%E9%AB%94/"
+                // alert("traditional_chinese");
+                openlink(clickData.selectionText, url_head);
+            }
+        });
+
+    }
+});
---
+++ popup.js
@@ -0,0 +1,86 @@
+
+function getCurrentTabUrl(callback) {
+  // Query filter to be passed to chrome.tabs.query - see
+  // https://developer.chrome.com/extensions/tabs#method-query
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, (tabs) => {
+    // chrome.tabs.query invokes the callback with a list of tabs that match the
+    // query. When the popup is opened, there is certainly a window and at least
+    // one tab, so we can safely assume that |tabs| is a non-empty array.
+    // A window can only have one active tab at a time, so the array consists of
+    // exactly one tab.
+    var tab = tabs[0];
+
+    // A tab is a plain object that provides information about the tab.
+    // See https://developer.chrome.com/extensions/tabs#type-Tab
+    var url = tab.url;
+
+    // tab.url is only available if the "activeTab" permission is declared.
+    // If you want to see the URL of other tabs (e.g. after removing active:true
+    // from |queryInfo|), then the "tabs" permission is required to see their
+    // "url" properties.
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+
+    callback(url);
+  });
+
+  // Most methods of the Chrome extension APIs are asynchronous. This means that
+  // you CANNOT do something like this:
+  //
+  // var url;
+  // chrome.tabs.query(queryInfo, (tabs) => {
+  //   url = tabs[0].url;
+  // });
+  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+}
+
+// main event
+document.addEventListener('DOMContentLoaded', () => {
+  getCurrentTabUrl((url) => {
+    
+    //確認cs_language 目前是中文還是英文 調整select 的value
+    //預設都是中文
+    chrome.storage.sync.get(['cs_language'], function(result) {
+            var tmp = result.cs_language
+            var dropdown = document.getElementById('dropdown');
+
+            if(tmp == "english"){
+              dropdown.value = "english"
+            }else{
+              dropdown.value = "traditional_chinese"
+            }
+    });
+
+
+    //設定中文還是英文
+    var dropdown = document.getElementById('dropdown');
+    
+    dropdown.addEventListener('change', () => {
+      if(dropdown.value == "traditional_chinese"){
+        //儲存到 storage 
+        //要特別注意{ksy: url_head} key 是不需要單引號，但value 需要。
+        chrome.storage.sync.set({cs_language: dropdown.value}, function() {
+          //這個log基本上沒有公用
+          console.log('Value is set to ' + dropdown.value);
+        });
+
+      }else if(dropdown.value == "english"){
+        chrome.storage.sync.set({cs_language: dropdown.value}, function() {
+          //alert('english');
+          console.log('Value is set to ' + dropdown.value);
+        });
+      }
+      
+    });
+
+  });
+});
+
+
+
+
+
